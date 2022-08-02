f683e941b73fd38fde33d09f5ea1d3e40549bd6e
Alexander <protonull@protonmail.com>
2022-08-02 21:37:46 +0100

    First version

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lawful
+
+Lawful is an in-browser Quill editor that handles raw Delta.
+
+## Front Matter
+
+The editor will accept front-matter, but it cannot be edited. Instead the front-matter will be re-exported with the document.
+
+## Attributions
+
+- favicon: [CC BY 3.0](https://thenounproject.com/icon/writing-web-page-3201283/)
+- jQuery & jQuery UI: [MIT](https://jquery.org/license/)
+- QuillJS: [BSD-3](https://github.com/quilljs/quill/blob/develop/LICENSE)
+- sanitize.css: [CC0-1.0](https://github.com/csstools/sanitize.css/blob/main/LICENSE.md)
+- gray-matter: [MIT](https://github.com/jonschlinkert/gray-matter/blob/master/LICENSE)
---
+++ lawful.js
@@ -0,0 +1,107 @@
+let frontmatter = null;
+
+window.quillEditor = new Quill(document.getElementById("editor"), {
+    theme: "snow",
+    modules: {
+        toolbar: [
+            [{"header": [1, 2, 3, 4, 5, false]}],
+            ["bold", "italic", "underline", "strike"],
+            [{"color": []}, {"background": []}],
+            [{"align": []}, {"indent": "-1"}, {"indent": "+1"}, {"list": "ordered"}, {"list": "bullet"}],
+            ["blockquote", "code-block"],
+            ["link", "image", "video"],
+            ["clean"]
+        ]
+    }
+});
+
+$(function() {
+    window.sourceDialog = $("<textarea></textarea>").dialog({
+        title: "Paste in your Quill document source below",
+        autoOpen: false,
+        height: 400,
+        width: 600,
+        modal: true,
+        buttons: {
+            Apply: function () {
+                const previousMatter = frontmatter;
+                let matter = parseGreyMatter(sourceDialog.val());
+                let contents;
+                try {
+                    contents = JSON.parse(matter.content);
+                }
+                catch (e) {
+                    sourceDialog.dialog("close");
+                    $(function() {
+                        $("<div><p>Could not parse that Quill document source!</p></div>").dialog({
+                            title: "Error!"
+                        });
+                    });
+                    return;
+                }
+                const previousSource = quillEditor.getContents();
+                try {
+                    quillEditor.setContents(contents);
+                }
+                catch (e) {
+                    quillEditor.setContents(previousSource);
+                    sourceDialog.dialog("close");
+                    $(function() {
+                        $("<div><p>Could not apply that Quill document source!</p></div>").dialog({
+                            title: "Error!"
+                        });
+                    });
+                    return;
+                }
+                frontmatter = matter.matter;
+                sourceDialog.dialog("close");
+            }
+        },
+        open: function () {
+            sourceDialog
+                .val("") // Clear the textarea
+                .outerWidth("100%")
+                .attr({ readonly: false })
+                .focus();
+        }
+    });
+    $("#import-button").button().on("click", function () {
+        sourceDialog.dialog("open");
+    });
+
+    window.exportDialog = $("<textarea></textarea>").dialog({
+        title: "Contained below is your Quill document's source",
+        autoOpen: false,
+        height: 400,
+        width: 600,
+        modal: true,
+        buttons: {
+            "Select All": function () {
+                exportDialog.select();
+            },
+            Close: function () {
+                exportDialog.dialog("close");
+            }
+        },
+        open: function () {
+            let stringifiedContents = JSON.stringify(quillEditor.getContents() ?? {}, null, 4);
+            if (frontmatter) {
+                stringifiedContents
+                    = "---"
+                    + (!frontmatter.startsWith("\n") ? "\n" : "")
+                    + frontmatter
+                    + (!frontmatter.endsWith("\n") ? "\n" : "")
+                    + "---"
+                    + "\n"
+                    + stringifiedContents;
+            }
+            exportDialog
+                .val(stringifiedContents)
+                .outerWidth("100%")
+                .attr({ readonly: true });
+        }
+    });
+    $("#export-button").button().on("click", function () {
+        exportDialog.dialog("open");
+    });
+});
